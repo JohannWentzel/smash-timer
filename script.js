bb54100fc6ea7b26d3b9385fe34b7032683cedd8
Johann Wentzel <wentzel142@gmail.com>
2018-06-14 22:06:01 -0700

    Initial functionality complete

---
+++ script.js
@@ -0,0 +1,19 @@
+var releaseDate = new Date("Dec 7, 2018").getTime();
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    
+    var distance = releaseDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("timetext").innerHTML = days + " DAYS<br>" + hours + " HOURS<br>" + minutes + " MINUTES<br>" + seconds + " SECONDS";
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timetext").innerHTML = "IT'S OUT";
+    }
+}, 1000)
